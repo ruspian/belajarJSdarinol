bf71841bb1625cae7121cde20504b94e69099eaa
ruspian <ruspianntb@gmail.com>
2024-12-28 15:35:45 +0800

    menuliskan javascript

---
+++ menulisJavascript/script.js
@@ -0,0 +1,8 @@
+document.write(
+  "ini adalah contoh menuliskan Javascript dengan file terpisah\n"
+);
+
+const nama = "Pian";
+
+document.getElementById("nama").innerHTML = "Selamat datang " + nama;
+console.log("Selamat datang " + nama);
